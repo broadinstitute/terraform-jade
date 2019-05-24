744330c1a4dae430a9a6304323c030542971da6f
Mark Sarcevicz <smark@broadinstitute.org>
2019-05-24 15:47:58 -0400

    removed dns creation when not in the dev project

--- jade-dns.tf
+++ jade-dns.tf
@@ -1,3 +1,8 @@
+data "google_dns_managed_zone" "dns_zone" {
+    provider     = "google.broad-jade-${var.env_project}"
+    project             = "${var.env_project}"
+    name                = "${var.env_project}"
+}
 #
 # jade Service - DNS A Record and Public IP
 #
@@ -10,9 +15,9 @@ resource "google_compute_global_address" "jade-k8-ip" {
 }
 
 resource "google_dns_record_set" "jade-a-dns" {
-  provider     = "google"
-  managed_zone = "${google_dns_managed_zone.dns_zone.name}"
-  name         = "jade-global.${google_dns_managed_zone.dns_zone.dns_name}"
+  provider     = "google.broad-jade-${var.env_project}"
+  managed_zone = "${data.google_dns_managed_zone.dns_zone.name}"
+  name         = "jade-global.${data.google_dns_managed_zone.dns_zone.dns_name}"
   type         = "A"
   ttl          = "300"
   rrdatas      = [ "${google_compute_global_address.jade-k8-ip.address}" ]
@@ -20,11 +25,11 @@ resource "google_dns_record_set" "jade-a-dns" {
 }
 
 resource "google_dns_record_set" "jade-cname-jade-dns" {
-    provider = "google"
-    managed_zone = "${google_dns_managed_zone.dns_zone.name}"
-    name = "jade.${google_dns_managed_zone.dns_zone.dns_name}"
+    provider = "google.broad-jade-${var.env_project}"
+    managed_zone = "${data.google_dns_managed_zone.dns_zone.name}"
+    name = "jade.${data.google_dns_managed_zone.dns_zone.dns_name}"
     type = "CNAME"
     ttl = "300"
-    rrdatas = ["jade-global.${google_dns_managed_zone.dns_zone.dns_name}"]
+    rrdatas = ["jade-global.${data.google_dns_managed_zone.dns_zone.dns_name}"]
     depends_on = ["google_dns_record_set.jade-a-dns"]
 }
