d42538cf370d4de66832c24a424d36362acf5da9
Mark Sarcevicz <smark@broadinstitute.org>
2019-06-19 13:22:19 -0400

    added vault secrets for sql

--- jade-cloudsql-db-users.tf
+++ jade-cloudsql-db-users.tf
@@ -4,6 +4,7 @@ resource "google_sql_database" "jade-datarepo-db" {
     instance = "${google_sql_database_instance.jade_100_postgres.name}"
     charset = "UTF8"
     collation = "en_US.UTF8"
+    depends_on = ["google_sql_database_instance.jade_100_postgres"]
 }
 
 resource "google_sql_database" "jade-stairway-db" {
@@ -12,4 +13,40 @@ resource "google_sql_database" "jade-stairway-db" {
     instance = "${google_sql_database_instance.jade_100_postgres.name}"
     charset = "UTF8"
     collation = "en_US.UTF8"
+    depends_on = ["google_sql_database_instance.jade_100_postgres"]
+}
+
+resource "random_id" "jade-db-password" {
+    byte_length = 16
+    depends_on = ["google_sql_database_instance.jade_100_postgres"]
+}
+
+resource "google_sql_user" "jade-db-user" {
+    name = "drmanager"
+    password =  "${random_id.jade-db-password.hex}"
+    project = "${var.project}"
+    instance = "${google_sql_database_instance.jade_100_postgres.name}"
+    depends_on = ["google_sql_database_instance.jade_100_postgres"]
+}
+
+resource "vault_generic_secret" "jade-db-login-secret" {
+    path = "secret/dsde/datarepo/dev/db-secrets.json"
+    data_json = <<EOT
+{
+  "cloud_sql_proxy":{
+      "instance_name": "${google_sql_database_instance.jade_100_postgres.name}",
+      "connection_name": "${google_sql_database_instance.jade_100_postgres.connection_name}"
+  },
+  "datarepo":{
+    "db_name": "${google_sql_database.jade-datarepo-db.name}",
+    "username": "${google_sql_user.jade-db-user.name}",
+    "password": "${google_sql_user.jade-db-user.password}"
+  },
+  "stairway":{
+    "db_name": "${google_sql_database.jade-stairway-db.name}",
+    "username": "${google_sql_user.jade-db-user.name}",
+    "password": "${google_sql_user.jade-db-user.password}"
+    }
+}
+EOT
 }
