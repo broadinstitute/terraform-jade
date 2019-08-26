2ff11ce23052153b032754ad8bf9f471ccc56fc1
Mark Sarcevicz <47335998+smark-broad@users.noreply.github.com>
2019-08-26 13:02:56 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -43,35 +43,3 @@ resource "google_dns_managed_zone" "dns_zone" {
   depends_on  = [module.enable-services]
 }
 
-
-
-
-# Create a NAT router for k8s so nodes can interact with external services as a static IP.
-
-resource "google_compute_router" "router" {
-  name = "${var.k8s_cluster_name}-router"
-  project = var.project
-  network = google_compute_network.jade-network.self_link
-
-  bgp {
-    asn = 64514
-  }
-}
-
-resource "google_compute_address" "nat-address" {
-  count = 2
-  name = "${var.k8s_cluster_name}-nat-external-${count.index}"
-  project = var.project
-  depends_on = [module.enable-services]
-}
-
-resource "google_compute_router_nat" "nat" {
-  name = "${var.k8s_cluster_name}-nat-1"
-  project = var.project
-  router = google_compute_router.router.name
-
-  nat_ip_allocate_option = "MANUAL_ONLY"
-  nat_ips = google_compute_address.nat-address[*].self_link
-
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
