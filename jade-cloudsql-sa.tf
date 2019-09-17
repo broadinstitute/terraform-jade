096fdaf9cf34aa64a3be1d25eaa4269a1a84cd5f
Mark Sarcevicz <smark@broadinstitute.org>
2019-09-17 11:51:32 -0400

    added sql backup service account

--- jade-cloudsql-sa.tf
+++ jade-cloudsql-sa.tf
@@ -17,3 +17,24 @@ resource "vault_generic_secret" "sql-sa-key-secret" {
     path      = "secret/dsde/datarepo/${var.env}/proxy-sa-${var.suffix}.json"
     data_json = base64decode(google_service_account_key.sql-sa-key.private_key)
 }
+
+#separate backup account
+resource "google_service_account" "sql-backup-account" {
+  account_id   = "sql-backup-sa"
+  display_name = "sql backup service account"
+}
+
+resource "google_service_account_key" "sql-backup-key" {
+    service_account_id = google_service_account.sql-backup-account.name
+}
+
+resource "google_project_iam_member" "sql-backup-role" {
+    project = var.project
+    role    = "roles/cloudsql.client"
+    member  = "serviceAccount:${google_service_account.sql-backup-account.email}"
+}
+
+resource "vault_generic_secret" "sql-backup-key-secret" {
+    path      = "secret/dsde/datarepo/${var.env}/sql-backup-sa-${var.suffix}.json"
+    data_json = base64decode(google_service_account_key.sql-backup-key.private_key)
+}
