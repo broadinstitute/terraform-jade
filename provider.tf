69840ae33015d62fcf2d565b69a24f62726f470f
Mark Sarcevicz <smark@broadinstitute.org>
2019-12-17 15:39:03 -0500

    edited providera and api module

--- provider.tf
+++ provider.tf
@@ -2,22 +2,14 @@ provider "google" {
     credentials = file("${var.env}_svc.json")
     project = var.project
     region  = var.region
-    version     = "~>2.20.0"
+    version     = "~> 3.2.0"
 }
 
 provider "google-beta" {
     credentials = file("${var.env}_svc.json")
     project     = var.project
     region      = var.region
-    version     = "~>2.20.0"
-}
-
-provider "google" {
-    alias       = "broad-jade"
-    credentials = file("${var.env}_svc.json")
-    project     = var.env_project
-    region      = var.region
-    version     = "~>2.20.0"
+    version     = "~> 3.2.0"
 }
 
 provider "vault" {
