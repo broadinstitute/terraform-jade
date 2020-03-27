a284bcebd2608e8df28133a70b95a5de1851931d
Mark Sarcevicz <smark@broadinstitute.org>
2020-03-27 10:21:28 -0400

    removed dup

--- network.tf
+++ network.tf
@@ -32,11 +32,3 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.sql_private_ip_address.name]
 }
-
-resource "google_dns_managed_zone" "dns_zone" {
-  provider   = google
-  count      = "${var.env == var.suffix ? "1" : "0"}"
-  name       = "datarepo-${var.env}"
-  dns_name   = "datarepo-${var.env}.broadinstitute.org."
-  depends_on = [module.enable-services]
-}
