007a8e5523f7c3153a09df616d135c33560e3a85
Mark Sarcevicz <smark@broadinstitute.org>
2020-05-12 13:54:01 -0400

    added new node pool

--- jade-us-central-k8s.tf
+++ jade-us-central-k8s.tf
@@ -14,7 +14,8 @@ module "k8s-master" {
     cluster_secondary_range_name  = "pods"
     services_secondary_range_name = "services"
   }
-  private_ipv4_cidr_block = var.private_ipv4_cidr_block
+  private_ipv4_cidr_block  = var.private_ipv4_cidr_block
+  enable_workload_identity = var.enable_workload_identity
 }
 
 module "k8s-nodes" {
@@ -22,12 +23,28 @@ module "k8s-nodes" {
   source       = "github.com/broadinstitute/terraform-shared.git//terraform-modules/k8s-node-pool?ref=k8s-cluster-monitoring-0.0.3-tf-0.12"
   dependencies = [module.enable-services, module.k8s-master]
 
-  name         = var.node_name
-  master_name  = var.master_name
-  location     = var.region
-  node_count   = var.node_count
-  machine_type = var.machine_type
-  disk_size_gb = var.disk_size_gb
-  labels       = var.node_labels
-  tags         = var.node_tags
+  name                     = var.node_name
+  master_name              = var.master_name
+  location                 = var.region
+  node_count               = var.node_count
+  machine_type             = var.machine_type
+  disk_size_gb             = var.disk_size_gb
+  labels                   = var.node_labels
+  tags                     = var.node_tags
+}
+
+module "k8s-nodes" {
+  # terraform-shared repo
+  source       = "github.com/broadinstitute/terraform-shared.git//terraform-modules/k8s-node-pool?ref=k8s-cluster-monitoring-0.0.3-tf-0.12"
+  dependencies = [module.enable-services, module.k8s-master]
+
+  name                     = "${var.node_name}-2"
+  master_name              = var.master_name
+  location                 = var.region
+  node_count               = var.node_count
+  machine_type             = var.machine_type
+  disk_size_gb             = var.disk_size_gb
+  labels                   = var.node_labels
+  tags                     = var.node_tags
+  enable_workload_identity = var.enable_workload_identity
 }
