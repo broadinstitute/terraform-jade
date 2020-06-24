a1a0326b56b144f99cd19aa93e79b89ce040a465
Mark Sarcevicz <smark@broadinstitute.org>
2020-06-24 16:58:38 -0400

    added api enabler

--- environments/perf/main.tf
+++ environments/perf/main.tf
@@ -1,3 +1,28 @@
+module "enable-services" {
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.3.0-tf-0.12"
+
+  providers = {
+    google.target = google-beta
+  }
+  project = var.google_project
+  services = [
+    "iamcredentials.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "genomics.googleapis.com",
+    "compute.googleapis.com",
+    "sqladmin.googleapis.com",
+    "dns.googleapis.com",
+    "sql-component.googleapis.com",
+    "monitoring.googleapis.com",
+    "sqladmin.googleapis.com",
+    "container.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "servicenetworking.googleapis.com"
+  ]
+}
+
 # gcp networking, k8 cluster
 module "core-infrastructure" {
   source = "github.com/broadinstitute/terraform-jade.git//modules/core-infrastructure?ref=ms-datarepomodule"
