3836f218ad77878ddeda859d20350ff2d30aad7c
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-08 10:53:24 -0400

    trying fix data

--- modules/datarepo-app/dns.tf
+++ modules/datarepo-app/dns.tf
@@ -1,25 +1,19 @@
 # Global Ip, CNAME, A Record
 data google_dns_managed_zone dns_zone {
   count = var.enable ? 1 : 0
-
   provider   = google-beta.datarepo-dns
   name       = var.dns_zone
   depends_on = [var.dependencies]
 }
 
-locals {
-  zone_gcp_name = var.enable ? data.google_dns_managed_zone.dns_zone[0].name : null
-  zone_dns_name = var.enable ? data.google_dns_managed_zone.dns_zone[0].dns_name : null
-}
-
 module datarepo_dns_names {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/external-dns?ref=ms-dns-fix"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/external-dns?ref=external-dns-0.0.3"
   providers = {
     google.ip  = google-beta.target,
     google.dns = google-beta.datarepo-dns
   }
-  dependencies  = var.dependencies
-  zone_gcp_name = local.zone_gcp_name
-  zone_dns_name = local.zone_dns_name
+  dependencies  = [data.google_dns_managed_zone.dns_zone]
+  zone_gcp_name = data.google_dns_managed_zone.dns_zone.name
+  zone_dns_name = data.google_dns_managed_zone.dns_zone.dns_name
   dns_names     = var.dns_names
 }
--- modules/multi-tenant/dns.tf
+++ modules/multi-tenant/dns.tf
@@ -1,18 +1,21 @@
 # Global Ip, CNAME, A Record
 data google_dns_managed_zone dns_zone {
+  count = var.enable ? 1 : 0
   provider = google-beta.datarepo-dns
   name     = var.dns_zone
+  depends_on = [var.dependencies]
 }
 
 module datarepo_dns_names {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/external-dns?ref=ms-dns-fix"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/external-dns?ref=external-dns-0.0.3"
   providers = {
     google.ip  = google-beta.target,
     google.dns = google-beta.datarepo-dns
   }
 
-  dependencies  = var.dependencies
+  dependencies  = [data.google_dns_managed_zone.dns_zone]
   zone_gcp_name = data.google_dns_managed_zone.dns_zone.name
   zone_dns_name = data.google_dns_managed_zone.dns_zone.dns_name
   dns_names     = var.dns_names
+
 }
