4063cc1f884d12b34df5446c304d1a15a1bab9d7
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-27 14:55:11 -0400

    used data qurery

--- modules/datarepo-app/dns.tf
+++ modules/datarepo-app/dns.tf
@@ -19,7 +19,7 @@ resource google_dns_record_set a_dns {
   type = "A"
   ttl = "300"
 
-  managed_zone = var.zone_gcp_name
+  managed_zone = data.google_dns_managed_zone.dns_zone[0].name
   name = "${var.dns_name}-global.${data.google_dns_managed_zone.dns_zone[0].dns_name}"
   rrdatas = [google_compute_global_address.global_ip_address[0].address]
   depends_on = [var.dependencies,data.google_dns_managed_zone.dns_zone]
@@ -31,7 +31,7 @@ resource google_dns_record_set cname_dns {
   type = "CNAME"
   ttl = "300"
 
-  managed_zone = var.zone_gcp_name
+  managed_zone = data.google_dns_managed_zone.dns_zone[0].name
   name = "${var.dns_name}.${data.google_dns_managed_zone.dns_zone[0].dns_name}"
   rrdatas = [google_dns_record_set.a_dns[0].name]
   depends_on = [var.dependencies,data.google_dns_managed_zone.dns_zone]
