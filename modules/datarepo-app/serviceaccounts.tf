59255f3aff6e8413b63eb569f5494b6c947db6de
Florian Boulnois <fboulnoi@broadinstitute.org>
2020-10-14 11:59:58 -0400

    Move around sections

--- modules/datarepo-app/serviceaccounts.tf
+++ modules/datarepo-app/serviceaccounts.tf
@@ -61,49 +61,6 @@ resource "google_project_iam_member" "api_sa_role" {
   depends_on = [var.dependencies]
 }
 
-## test-runner-sa
-resource "google_service_account" "datarepo_test_runner_sa" {
-  count        = var.enable ? 1 : 0
-  provider     = google.target
-  project      = var.google_project
-  account_id   = "${local.service}-${local.owner}-test-runner"
-  display_name = "${local.service}-${local.owner}-test-runner"
-  depends_on   = [var.dependencies]
-}
-
-resource "google_project_iam_member" "test_runner_sa_role" {
-  count = var.enable ? length(local.test_runner_roles) : 0
-
-  provider   = google.target
-  project    = var.google_project
-  role       = local.test_runner_roles[count.index]
-  member     = "serviceAccount:${google_service_account.datarepo_test_runner_sa[0].email}"
-  depends_on = [var.dependencies]
-}
-
-## vault write test runner
-resource "google_service_account_key" "test_runner_sa_key" {
-  count = var.enable ? 1 : 0
-
-  provider           = google.target
-  service_account_id = google_service_account.datarepo_test_runner_sa[0].name
-  depends_on         = [var.dependencies]
-}
-
-resource "vault_generic_secret" "test_runner_sa_key" {
-  count = var.enable ? 1 : 0
-
-  depends_on = [var.dependencies]
-  provider   = vault.target
-  path       = "${local.vault_path}/test-runner-sa"
-
-  data_json = <<EOT
-{
-  "key": "${google_service_account_key.test_runner_sa_key[0].private_key}"
-}
-EOT
-}
-
 ##
 # vault write api
 resource "google_service_account_key" "api_sa_key" {
@@ -124,6 +81,49 @@ resource "vault_generic_secret" "api_sa_key" {
   data_json = <<EOT
 {
   "key": "${google_service_account_key.api_sa_key[0].private_key}"
+}
+EOT
+}
+
+## test-runner-sa
+resource "google_service_account" "datarepo_test_runner_sa" {
+  count        = var.enable ? 1 : 0
+  provider     = google.target
+  project      = var.google_project
+  account_id   = "${local.service}-${local.owner}-test-runner"
+  display_name = "${local.service}-${local.owner}-test-runner"
+  depends_on   = [var.dependencies]
+}
+
+resource "google_project_iam_member" "test_runner_sa_role" {
+  count = var.enable ? length(local.test_runner_roles) : 0
+
+  provider   = google.target
+  project    = var.google_project
+  role       = local.test_runner_roles[count.index]
+  member     = "serviceAccount:${google_service_account.datarepo_test_runner_sa[0].email}"
+  depends_on = [var.dependencies]
+}
+
+## vault write test-runner-sa
+resource "google_service_account_key" "test_runner_sa_key" {
+  count = var.enable ? 1 : 0
+
+  provider           = google.target
+  service_account_id = google_service_account.datarepo_test_runner_sa[0].name
+  depends_on         = [var.dependencies]
+}
+
+resource "vault_generic_secret" "test_runner_sa_key" {
+  count = var.enable ? 1 : 0
+
+  depends_on = [var.dependencies]
+  provider   = vault.target
+  path       = "${local.vault_path}/test-runner-sa"
+
+  data_json = <<EOT
+{
+  "key": "${google_service_account_key.test_runner_sa_key[0].private_key}"
 }
 EOT
 }
