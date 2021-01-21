ccfee8f86d8aba5d60cfc808a368f83e976ce98a
Mark Sarcevicz <smark@broadinstitute.org>
2021-01-21 14:59:59 -0500

    applied

--- datarepo/modules/core-infrastructure/kubernetes.tf
+++ datarepo/modules/core-infrastructure/kubernetes.tf
@@ -1,6 +1,6 @@
 module "k8s-master" {
   # terraform-shared repo
-  source     = "github.com/broadinstitute/terraform-shared.git//terraform-modules/k8s-master?ref=k8s-cluster-monitoring-0.0.3-tf-0.12"
+  source     = "github.com/broadinstitute/terraform-shared.git//terraform-modules/k8s-master?ref=DR-1589-ms-cis-bestpractices"
   depends_on = [google_compute_network.network, google_compute_subnetwork.subnetwork]
 
   name                     = local.master_name
@@ -14,8 +14,10 @@ module "k8s-master" {
     cluster_secondary_range_name  = "pods"
     services_secondary_range_name = "services"
   }
-  private_ipv4_cidr_block  = var.private_ipv4_cidr_block
-  enable_workload_identity = var.enable_workload_identity
+  private_ipv4_cidr_block     = var.private_ipv4_cidr_block
+  enable_workload_identity    = var.enable_workload_identity
+  enable_shielded_nodes       = var.enable_shielded_nodes
+  enable_binary_authorization = var.enable_binary_authorization
 }
 
 module "k8s-nodes" {
@@ -33,4 +35,23 @@ module "k8s-nodes" {
   labels                   = local.node_labels
   tags                     = local.node_tags
   enable_workload_identity = var.enable_workload_identity
+}
+
+module "k8s-cis-nodes" {
+  # terraform-shared repo
+  source     = "github.com/broadinstitute/terraform-shared.git//terraform-modules/k8s-node-pool?ref=DR-1589-ms-cis-bestpractices"
+  for_each   = var.node_regions
+  depends_on = [module.k8s-master,google_service_account.node_pool]
+
+  name                     = "${each.key}-cis"
+  master_name              = local.master_name
+  location                 = each.value.region
+  node_count               = var.node_count
+  machine_type             = var.machine_type
+  disk_size_gb             = var.disk_size_gb
+  labels                   = local.node_labels
+  tags                     = local.node_tags
+  enable_workload_identity = var.enable_workload_identity
+  service_account          = google_service_account.node_pool.email
+  enable_secure_boot       = var.enable_secure_boot
 }
