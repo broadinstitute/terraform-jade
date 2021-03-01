13f4144376bf858e2b6bc2ebddbc7d555dac91cf
Mark Sarcevicz <>
2021-03-01 15:27:28 -0500

    added iam permissions for kms encryption and decrpytion

--- datarepo/modules/core-infrastructure/serviceaccount.tf
+++ datarepo/modules/core-infrastructure/serviceaccount.tf
@@ -1,3 +1,6 @@
+data "google_project" "project" {
+}
+
 resource "google_service_account" "node_pool" {
   account_id   = "gke-node-sa"
   display_name = "gke service account"
@@ -8,6 +11,17 @@ resource "google_project_iam_member" "node_pool" {
   project = var.google_project
   role    = element(local.node_pool_gke_roles, count.index)
   member  = "serviceAccount:${google_service_account.node_pool.email}"
+}
+
+
+
+resource "google_project_iam_binding" "kubernetes_engine_kms_access" {
+  project = var.google_project
+  role    = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
+  members = [
+    "serviceAccount:service-${data.google_project.project.number}@container-engine-robot.iam.gserviceaccount.com",
+  ]
 }
 
 locals {
