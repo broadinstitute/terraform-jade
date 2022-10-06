ddac4634f1c786362349d3a155ca96a833f542d9
nmalfroy <nmalfroy@broadinstitute.org>
2022-10-05 20:09:26 -0400

    Upgrade logsink module

--- old/logsink.tf
+++ old/logsink.tf
@@ -1,11 +1,12 @@
 
 module "user-activity-sinks" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/gcs_bq_log_sink?ref=sinks-0.0.9"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/gcs_bq_log_sink?ref=sinks-0.0.13"
 
   providers = {
     google = google
   }
 
+  enable           = var.user_activity_enable_pubsub || var.user_activity_enable_bigquery
   enable_pubsub    = var.user_activity_enable_pubsub
   enable_bigquery  = var.user_activity_enable_bigquery
   owner            = "jade"
@@ -16,15 +17,16 @@ module "user-activity-sinks" {
 }
 
 module "performance-log-sinks" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/gcs_bq_log_sink?ref=sinks-0.0.9"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/gcs_bq_log_sink?ref=sinks-0.0.13"
 
   providers = {
     google.target      = google
     google-beta.target = google-beta
   }
 
-  enable_pubsub    = 0
-  enable_bigquery  = 1
+  enable           = true
+  enable_pubsub    = false
+  enable_bigquery  = true
   owner            = "jade"
   application_name = "datarepo"
   log_filter       = "resource.type=\"k8s_container\" \"PerformanceLogger\""
